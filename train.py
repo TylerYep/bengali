eaf5905019ff9771a95ea78211422616a778f3fe
Tyler Yep <tyep@stanford.edu>
2019-12-23 11:29:55 -0800

    remove tqdm from train

--- train.py
+++ train.py
@@ -3,17 +3,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-from torchsummary import summary
-# if torch.cuda.is_available():
-#     from tqdm import tqdm_notebook as tqdm
-# else:
-from tqdm import tqdm
+# from torchsummary import summary
 
 import util
-from util import AverageMeter
+# from util import AverageMeter
 from dataset import load_data
 from models import ResNet18
-from viz import visualize
+# from viz import visualize
 
 
 def train_model(args, model, criterion, train_loader, optimizer, epoch, writer):
@@ -24,18 +20,69 @@ def train_model(args, model, criterion, train_loader, optimizer, epoch, writer):
 
     running_acc, running_loss = 0.0, 0.0
     acc, loss = 0.0, 0.0
-    with tqdm(desc='Batch', total=len(train_loader), ncols=120, position=1, leave=True) as pbar:
-        for i, (data, labels1, labels2, labels3) in enumerate(train_loader):
-            data = data.to(device).unsqueeze(1).float()
+    for i, (data, labels1, labels2, labels3) in enumerate(train_loader):
+        if i % 500 == 0:
+            print(i)
+        data = data.to(device).unsqueeze(1).float()
+        labels1 = labels1.to(device)
+        labels2 = labels2.to(device)
+        labels3 = labels3.to(device)
+        optimizer.zero_grad()
+
+        # if args.visualize:
+        #     visualize(data, labels1)
+
+        outputs1, outputs2, outputs3 = model(data)
+        loss1 = criterion(outputs1, labels1)
+        loss2 = criterion(outputs2, labels2)
+        loss3 = criterion(outputs3, labels3)
+
+        total_loss = loss1.item() + loss2.item() + loss3.item()
+        output1_diff = (outputs1.argmax(1) == labels1).float().mean()
+        output2_diff = (outputs2.argmax(1) == labels2).float().mean()
+        output3_diff = (outputs3.argmax(1) == labels3).float().mean()
+
+        running_loss += total_loss
+        running_acc += output1_diff
+        running_acc += output2_diff
+        running_acc += output3_diff
+
+        loss += total_loss
+        acc += output1_diff
+        acc += output2_diff
+        acc += output3_diff
+
+        (loss1 + loss2 + loss3).backward()
+        optimizer.step()
+
+        num_steps = (epoch-1) * len(train_loader) + i
+
+        if i % args.log_interval == 0:
+            writer.add_scalar('training loss', loss / args.log_interval, num_steps)
+            writer.add_scalar('training accuracy', acc / args.log_interval, num_steps)
+            acc, loss = 0.0, 0.0
+
+
+    return 0
+
+
+def validate_model(args, model, criterion, val_loader, epoch, writer):
+    model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    acc, loss = 0.0, 0.0
+    running_acc, running_loss = 0.0, 0.0
+    with torch.no_grad():
+        for i, (data, labels1, labels2, labels3) in enumerate(val_loader):
+            if i % 500 == 0:
+                print(i)
+            data = data.to(device)
             labels1 = labels1.to(device)
             labels2 = labels2.to(device)
             labels3 = labels3.to(device)
-            optimizer.zero_grad()
 
-            if args.visualize:
-                visualize(data, labels1)
-
-            outputs1, outputs2, outputs3 = model(data)
+            outputs1, outputs2, outputs3 = model(data.unsqueeze(1).float())
             loss1 = criterion(outputs1, labels1)
             loss2 = criterion(outputs2, labels2)
             loss3 = criterion(outputs3, labels3)
@@ -55,68 +102,12 @@ def train_model(args, model, criterion, train_loader, optimizer, epoch, writer):
             acc += output2_diff
             acc += output3_diff
 
-            (loss1 + loss2 + loss3).backward()
-            optimizer.step()
-
-            num_steps = (epoch-1) * len(train_loader) + i
+            num_steps = (epoch-1) * len(val_loader) + i
 
             if i % args.log_interval == 0:
-                writer.add_scalar('training loss', loss / args.log_interval, num_steps)
-                writer.add_scalar('training accuracy', acc / args.log_interval, num_steps)
+                writer.add_scalar('val loss', loss / args.log_interval, num_steps)
+                writer.add_scalar('val accuracy', acc / args.log_interval, num_steps)
                 acc, loss = 0.0, 0.0
-
-            pbar.set_postfix({'Accuracy': f'{running_acc / (len(train_loader)*3):.5f}',
-                              'Loss': running_loss / len(train_loader)})
-            pbar.update()
-
-    return 0
-
-
-def validate_model(args, model, criterion, val_loader, epoch, writer):
-    model.eval()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
-
-    acc, loss = 0.0, 0.0
-    running_acc, running_loss = 0.0, 0.0
-    with torch.no_grad():
-        with tqdm(desc='Batch', total=len(val_loader), ncols=120, position=1, leave=True) as pbar:
-            for i, (data, labels1, labels2, labels3) in enumerate(val_loader):
-                data = data.to(device)
-                labels1 = labels1.to(device)
-                labels2 = labels2.to(device)
-                labels3 = labels3.to(device)
-
-                outputs1, outputs2, outputs3 = model(data.unsqueeze(1).float())
-                loss1 = criterion(outputs1, labels1)
-                loss2 = criterion(outputs2, labels2)
-                loss3 = criterion(outputs3, labels3)
-
-                total_loss = loss1.item() + loss2.item() + loss3.item()
-                output1_diff = (outputs1.argmax(1) == labels1).float().mean()
-                output2_diff = (outputs2.argmax(1) == labels2).float().mean()
-                output3_diff = (outputs3.argmax(1) == labels3).float().mean()
-
-                running_loss += total_loss
-                running_acc += output1_diff
-                running_acc += output2_diff
-                running_acc += output3_diff
-
-                loss += total_loss
-                acc += output1_diff
-                acc += output2_diff
-                acc += output3_diff
-
-                num_steps = (epoch-1) * len(val_loader) + i
-
-                if i % args.log_interval == 0:
-                    writer.add_scalar('val loss', loss / args.log_interval, num_steps)
-                    writer.add_scalar('val accuracy', acc / args.log_interval, num_steps)
-                    acc, loss = 0.0, 0.0
-
-                pbar.set_postfix({'Accuracy': f'{running_acc / (len(val_loader)*3):.5f}',
-                                'Loss': running_loss / len(val_loader)})
-                pbar.update()
 
     return running_loss
 
